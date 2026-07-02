42e88c0c28f0bf2a159a6ef252695f68f251c45b
Анастасия Филиппова <spinastya@yandex.ru>
2026-07-02 15:55:15 +0300

    test: fix spring boot test starter version and switch integration tests to @AutoConfigureMockMvc

--- src/test/java/com/example/messenger/UserControllerIntegrationTests.java
+++ src/test/java/com/example/messenger/UserControllerIntegrationTests.java
@@ -1,32 +1,29 @@
 package com.example.messenger;
 
 import com.example.messenger.controller.UserController;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest // Запускаем весь сервер
-@ActiveProfiles("test") // Включаем базу H2 из application-test.properties
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class UserControllerIntegrationTests {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private UserController userController;
-
-    @BeforeEach
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-    }
 
     @Test
     public void testLoginConvertsToLowerCaseAndRegistersInH2() throws Exception {
