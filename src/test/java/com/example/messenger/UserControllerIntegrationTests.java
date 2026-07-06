04d0defef1921fa9c30d10bf540621e0f057987e
Анастасия Филиппова <spinastya@yandex.ru>
2026-07-06 14:31:44 +0300

    test: mock push service in integration tests safely

--- src/test/java/com/example/messenger/UserControllerIntegrationTests.java
+++ src/test/java/com/example/messenger/UserControllerIntegrationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,10 @@ public class UserControllerIntegrationTests {
 
     @Autowired
     private UserController userController;
+
+    @MockBean
+    private PushNotificationService pushNotificationService; // 🟢 Спринг заменит реальный сервис пушей на пустышку во время тестов!
+
 
     @Test
     public void testLoginConvertsToLowerCaseAndRegistersInH2() throws Exception {
